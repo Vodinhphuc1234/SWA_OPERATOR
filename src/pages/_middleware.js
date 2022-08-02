e222c212437e6708e100f951feec8098d347f880
vodinhphuc1234 <phucatoniegriezman@gmail.com>
2022-08-02 21:18:19 +0700

    mw redirect -> rewrite

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -1,19 +1,22 @@
 import { NextResponse } from "next/server";
 
 export default function middleware(req) {
-  const redirect = process.env.NODE_ENV == "development" ? "http://127.0.0.1:3001" : "https://swa-operator.vercel.app";
-  if (!req.url.includes("/login")) {
+  const redirect =
+    process.env.NODE_ENV == "development"
+      ? "http://127.0.0.1:3001"
+      : "https://swa-operator.vercel.app";
+  if (!req.nextUrl.pathname.startsWith("/login")) {
     const token = req.cookies.token;
     if (token) {
       console.log("pass");
       return NextResponse.next();
     }
     console.log("not pass");
-    return NextResponse.redirect(redirect+"/login");
+    return NextResponse.rewrite(new URL(redirect + "/login", req.url));
   } else if (req.cookies.token) {
     console.log("pass");
-    return NextResponse.redirect(redirect);
+    return NextResponse.rewrite(new URL(redirect, req.url));
   }
   console.log("login");
-  return NextResponse.next()
+  return NextResponse.next();
 }
